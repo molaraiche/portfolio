034d36ff0a4c707e25d7535dee9e975d4d0b209d
molaraiche <molaraiche@molaraiche.com>
2022-08-10 01:37:31 +0100

    set navbar

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,57 +9,51 @@ const Skills = () => {
             I am<span className="titleColor">Skilled</span> at
           </h1>
           <div className="skillsHolder">
-            <div className="basics">
-              <div className="html">
-                <img
-                  src={require("../assets/skills/html.svg").default}
-                  alt="html"
-                />
-              </div>
-              <div className="css">
-                <img
-                  src={require("../assets/skills/css.svg").default}
-                  alt="css"
-                />
-              </div>
-              <div className="js">
-                <img
-                  src={require("../assets/skills/javascript.svg").default}
-                  alt="javascript"
-                />
-              </div>
+            <div className="html">
+              <img
+                src={require("../assets/skills/html.svg").default}
+                alt="html"
+              />
             </div>
-            <div className="libraries">
-              <div className="react">
-                <img
-                  src={require("../assets/skills/react.svg").default}
-                  alt="react"
-                />
-              </div>
-              <div className="sass">
-                <img
-                  src={require("../assets/skills/sass.svg").default}
-                  alt="sass"
-                />
-              </div>
-              <div className="git">
-                <img
-                  src={require("../assets/skills/git.svg").default}
-                  alt="git"
-                />
-              </div>
-              <div className="redux">
-                <img
-                  src={require("../assets/skills/redux.svg").default}
-                  alt="redux"
-                />
-              </div>
-              <div className="cy">
-                <img
-                  src={require("../assets/skills/cy.svg").default}
-                  alt="cy"
-                />
-              </div>
+            <div className="css">
+              <img
+                src={require("../assets/skills/css.svg").default}
+                alt="css"
+              />
+            </div>
+            <div className="js">
+              <img
+                src={require("../assets/skills/javascript.svg").default}
+                alt="javascript"
+              />
+            </div>
+
+            <div className="react">
+              <img
+                src={require("../assets/skills/react.svg").default}
+                alt="react"
+              />
+            </div>
+            <div className="sass">
+              <img
+                src={require("../assets/skills/sass.svg").default}
+                alt="sass"
+              />
+            </div>
+            <div className="git">
+              <img
+                src={require("../assets/skills/git.svg").default}
+                alt="git"
+              />
+            </div>
+            <div className="redux">
+              <img
+                src={require("../assets/skills/redux.svg").default}
+                alt="redux"
+              />
+            </div>
+            <div className="cy">
+              <img src={require("../assets/skills/cy.svg").default} alt="cy" />
             </div>
           </div>
         </div>
