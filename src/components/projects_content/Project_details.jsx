bb8850191783cada7a4063444bd24d47d0d9682e
molaraiche <molaraiche@molaraiche.com>
2022-08-10 01:45:05 +0100

    set navbar

---
+++ src/components/projects_content/Project_details.jsx
@@ -0,0 +1,6 @@
+import "/src/styles/projects_content/Project_details.css";
+const ProjectDetails = () => {
+  return <>ProjectDetails</>;
+};
+
+export default ProjectDetails;
---
+++ src/components/projects_content/Projects_card.jsx
@@ -0,0 +1,7 @@
+import "/src/styles/projects_content/ProjectsCard.css";
+
+const ProjectsCard = () => {
+  return <>projects card</>;
+};
+
+export default ProjectsCard;
