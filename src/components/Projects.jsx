70726f19af8dacaf279207503879bacf7692cfc5
molaraiche <molaraiche@molaraiche.com>
2022-08-16 18:25:42 +0100

    add crud app

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import "../styles/Projects.css";
+import CRUDAPP from "../assets/projects_images/CRUD_APP.png";
 const style = {
   position: "absolute",
   top: "50%",
@@ -37,7 +38,12 @@ const Projects = () => {
           <div className="prjcts">
             <div className="projectsHolder">
               <div className="partOne">
-                <div className="r" onClick={handleOpenOne}></div>
+                <img
+                  src={CRUDAPP}
+                  alt="crud app"
+                  className="r"
+                  onClick={handleOpenOne}
+                />
                 <div className="l" onClick={handleOpenThree}></div>
               </div>
               <div className="partTwo" onClick={handleOpenTwo}></div>
@@ -52,15 +58,81 @@ const Projects = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <img src="https://via.placeholder.com/600X400" alt="" />
-          <ul>
-            <li>one</li>
-            <li>two</li>
-            <li>three</li>
-          </ul>
-          <div className="details">
-            <a href="."> code</a>
-            <a href="."> Live Preview</a>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              padding: "20px",
+            }}
+          >
+            <button
+              style={{
+                padding: "10px",
+                fontWeight: 600,
+                border: "none",
+                background: "#73e1ff",
+              }}
+              onClick={handleCloseOne}
+            >
+              {" "}
+              X
+            </button>
+          </div>
+          <img src={CRUDAPP} alt="Crud App" />
+          <div
+            style={{
+              margin: "20px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <span style={{ textAlign: "left", fontWeight: 600 }}>
+              Technologies used :
+            </span>
+            <ul style={{ margin: "5px" }}>
+              <li style={{ margin: "5px" }}>React JS</li>
+              <li style={{ margin: "5px" }}>Material UI</li>
+              <li style={{ margin: "5px" }}>Firebase</li>
+            </ul>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center",
+              }}
+            >
+              <a
+                style={{
+                  textDecoration: "none",
+                  background: "#73e1ff",
+                  color: "#222",
+                  margin: "0 10px",
+                  padding: "10px 40px",
+                }}
+                href="https://github.com/molaraiche/firebase-crud-app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                code
+              </a>
+              <a
+                style={{
+                  textDecoration: "none",
+                  background: "#73e1ff",
+                  color: "#222",
+                  margin: "0 10px",
+                  padding: "10px 40px",
+                }}
+                href="https://crudapp.molaraiche.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Preview
+              </a>
+            </div>
           </div>
         </Box>
       </Modal>
